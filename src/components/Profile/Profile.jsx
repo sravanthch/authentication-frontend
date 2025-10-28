4416a07763ca1857feaa184e6be48a74776892fe
Sravanth Ch <sravanthch18@gmail.com>
2025-10-28 22:26:31 +0530

    Access LS Variables

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,16 +5,18 @@ import { Button } from "primereact/button";
 import "./Profile.css"; // create your own styles if needed
 
 const Profile = () => {
-  const { username, email, password } = useSelector((state) => state.user);
+  // const { username, email, password } = useSelector((state) => state.user);
+  const { password } = useSelector((state) => state.user);
+
 
   return (
     <div className="profile-container">
       <Card title="My Profile" className="profile-card">
         <div className="profile-detail">
-          <strong>Name:</strong> <span>{username}</span>
+          <strong>Name:</strong> <span>{localStorage.getItem('username')}</span>
         </div>
         <div className="profile-detail">
-          <strong>Email:</strong> <span>{email}</span>
+          <strong>Email:</strong> <span>{localStorage.getItem('email')}</span>
         </div>
         <div className="profile-detail">
           <strong>Password:</strong> <span>{'*'.repeat(password.length)}</span>
